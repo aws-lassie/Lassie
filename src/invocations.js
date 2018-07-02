f9f675bce6d198cf582c91f5923bab4342257748
Rebecca Leff <rebeccaleff@Rebeccas-MacBook-Pro.local>
2018-07-02 13:55:57 -0400

    Dashboard basics complete

---
+++ src/invocations.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './styles.css';
+
+class Invocations extends React.Component {
+  render() {
+    return (
+      <div class="invocations">
+        <h5>Invocations</h5>
+        </div>
+    )
+  }
+}
+
+export default Invocations;
+
--- src/stats.js
+++ src/stats.js
@@ -4,15 +4,18 @@ import styles from './styles.css';
 class Stats extends React.Component {
   render() {
     return (
-      <div >
-        <div class="statBoxes">
-          Stat 1
+      <div class="remainingTop">
+        <div class="stats">
+          Avg Wastage
+          <div class="wastage"> 80% </div>
         </div>  
-        <div class="statBoxes">
-          Stat 2
+        <div class="stats">
+          Avg CPU % of Memory
+          <div class="memory"> 20% </div>
         </div>
-        <div class="statBoxes">  
-          Stat 3
+        <div class="stats">  
+          Avg number of cold starts
+          <div class="cold"> 14 </div>
         </div>  
       </div>  
     )
